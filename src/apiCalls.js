4f7c579ea725f9abf19cba53a9536711eba66c99
Deanna Stevens <deanna.sofias@gmail.com>
2023-11-12 15:02:06 -0500

    Add getAllFetches function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,6 +10,18 @@ const getFetch = (data) => {
       return data;
     })
     .catch((error) => {
+      //display in error message as alert or in an element
       console.log(error, "<---Error")
     });
 };
+
+export const getAllFetches = (userId) => {
+  // let userId = 3 ? `http://localhost:3001/api/v1/travelers/${userId}` 
+  return Promise.all([
+    getFetch(`travelers/${userId}`),
+    getFetch('trips'),
+    getFetch('destinations'),
+  ])
+}
+
+
