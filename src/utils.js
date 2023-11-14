58f5a872f72b5523f3408627b667fab06b6b2fd2
Deanna Stevens <deanna.sofias@gmail.com>
2023-11-13 20:28:40 -0500

    Add calculateYearlyCost function

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,15 @@
+import { dropdownDestinations } from "./DOMupdates"
+import { currentTraveler, allDestinations } from "./scripts"
+
 export const filterTripsUser = (userId, trips) => {
   // console.log(userId, trips)
+  //
   return trips.filter(trip => trip.userID === userId) 
 }
 
 /*
 
-find values of the inputs 
-get destination list to populate in drop down menu
+let currentTraveler 
 
 let trip = {
   id: null,
@@ -19,46 +22,51 @@ let trip = {
 }
 //** trip keys have "" and look to be strings: JSON
 
-//trip as an object created from inputs of the form, 
+find values of the inputs
+trip as an object created from inputs of the form, 
+when jetSet button is clicked, 
 
 export const createTrip = () => {
+  trip.id = Date.now
+  trip.userID = currentTraveler.id
+  trip.destinationID = 
 
   return trip
 }
-
-export const findCostOneTrip(trip) {
-    const destination = destinations.find(place => place.id === trip.destinationID);
-    const travelers = trip.travelers;
-    const estimatedFlightCostPerPerson = destination.estimatedFlightCostPerPerson;
-    const estimatedLodgingCostPerDay = destination.estimatedLodgingCostPerDay;
-    const duration = trip.duration;
-    
-    const costOfTrip = ((travelers * estimatedFlightCostPerPerson) + (travelers * estimatedLodgingCostPerDay * duration)) * 1.1;
-    
-    return costOfTrip;
-}
-
-
-export const filterYearlyTrips = (userID) => {
-    //take in the array of trips for the userID
-    //filter them for this year - DATE
-}
-
-export const calculateYearlyCost = (yearlyTrips) => {
-    //on load fn
-    // refer to global destinations
-     yearlyTrips.reduce((total, trip) => {
-        destinations.find(element => {
-            if (element.id === trip.destinationID) {
-                total += (trip.travelers * element.estimatedFlightCostPerPerson) + (element.estimatedLodgingCostPerDay * trip.duration))
-            } 
-        })
-        return total
-    }, 0)
-    total += (total * .1);
-    console.log(total);
-    return total
-}
-
-
 */
+
+// export const findCostOneTrip(trip) {
+//     const destination = destinations.find(place => place.id === trip.destinationID);
+//     const travelers = trip.travelers;
+//     const estimatedFlightCostPerPerson = destination.estimatedFlightCostPerPerson;
+//     const estimatedLodgingCostPerDay = destination.estimatedLodgingCostPerDay;
+//     const duration = trip.duration;
+    
+//     const costOfTrip = ((travelers * estimatedFlightCostPerPerson) + (travelers * estimatedLodgingCostPerDay * duration)) * 1.1;
+    
+//     return costOfTrip;
+// }
+
+
+export const filterYearlyTrips = (userTrips) => {
+  const currentYear = new Date("11/11/2022").getFullYear()
+  console.log(currentYear)
+    const yearlyTrips = userTrips.filter(trip => new Date(trip.date).getFullYear() === currentYear)
+    console.log(yearlyTrips)
+    return yearlyTrips
+}
+
+export const calculateYearlyCost = (yearlyTrips, allDestinations) => {
+  const totalCost = yearlyTrips.reduce((total, trip) => {
+    const destination = allDestinations.find(element => element.id === trip.destinationID);
+    if (destination) {
+      total += (trip.travelers * destination.estimatedFlightCostPerPerson) + (destination.estimatedLodgingCostPerDay * trip.duration);
+    }
+    return total;
+  }, 0);
+  const finalCost = totalCost + (totalCost * 0.1);
+  console.log(finalCost);
+  return finalCost;
+};
+   
+
