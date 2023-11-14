10ab0e2a01f5da75094fc75a39fef7bab064a096
Deanna Stevens <deanna.sofias@gmail.com>
2023-11-13 23:45:53 -0500

    Add postnewTrip post request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -24,4 +24,15 @@ export const getAllFetches = (userId) => {
   ])
 }
 
+export const postnewTrip = (tripAPI, data) => {
+  return fetch(`http://localhost:3001/api/v1/${tripAPI}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error));
+};
+
+
 
